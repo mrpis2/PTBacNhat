04fa8e4335866f4a1246e2ed3841b02fced2f704
mrpis2 <warhammerprogamer@gmail.com>
2023-12-08 21:57:57 +0700

    PTbacNhat

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap gia tri cua a: ");
+        Double a = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Nhap gia tri cua b: ");
+        Double b = Convert.ToDouble(Console.ReadLine());
+        
+        if (a!=0)
+        {
+            Console.WriteLine("Pt co nghiem la: " + -b/a);
+        }
+        else
+        {
+            if (b!=0)
+            {
+                Console.WriteLine("PT vo nghiem");
+            }
+            else
+            {
+                Console.WriteLine("PT vo so nghiem");
+            }
+        }
+    }
+}
